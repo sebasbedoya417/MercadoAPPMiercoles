305267da8bdcf9d6ca9a08efa433f0e41f60087c
Rosalba <romemo15@gmail.com>
2025-02-26 10:24:03 -0500

    feat se crea menu de opciones en main.py

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 #En una lista de mercado
 nombreUsuario=None
 
+#Declarando las variables
+productos=[]
+producto={}
+
+#crear un menu de opciones
+print("*** MerqueoAPP ***")
+print("1. Agregar Producto a tu lista de mercado")
+print(" Presiona 5 para SALIR")
+
+opcion=100
+while opcion != 5:
+    opcion=int(input("Digita una opcion del menu: "))
+    
+    
