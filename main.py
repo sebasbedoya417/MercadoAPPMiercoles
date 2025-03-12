a1e78c9a524d9c1d387b1173b3a0242937bc61b2
Rosalba <romemo15@gmail.com>
2025-03-12 08:04:28 -0500

    feat se crea menu de opciones alimentando la op1

--- main.py
+++ main.py
@@ -9,10 +9,38 @@ producto={}
 #crear un menu de opciones
 print("*** MerqueoAPP ***")
 print("1. Agregar Producto a tu lista de mercado")
+print("2. Mostrar tu lista de mercado")
+print("3. Modificar tu lista de mercado")
+print("4. Retirar producto de tu lista de mercado")
 print(" Presiona 5 para SALIR")
 
 opcion=100
 while opcion != 5:
     opcion=int(input("Digita una opcion del menu: "))
+
+    if opcion == 1:
+        print("Creando la lista")
+        #Poblando listas y diccionarios en python 
+
+        #Asignando claves a un diccionario
+        producto["id"]=5
+        producto["nombre"]=input("Digita el nombre del producto: ")
+        producto["presentacion"]=input(" Digita la presentacion del producto: ")
+        producto["cantidad"]=int(input("Digita la cantidad: "))
+        producto["precio"]=int(input("Digita el precio del producto: "))
+
+        #Asignando a una lista un diccionario
+        productos.append(producto)
+        print(productos)
+
+
+    elif opcion == 2:
+        print("Mostrando la lista")
+    elif opcion == 3:
+        print("Modificando la lista")
+    elif opcion == 4:
+        print("Retirando un producto")
+    else:
+        print("Opcion invalida")
     
     
